9fcb27ceb4fbb630506c19b759e3e0afec4ccdb0
Tracy Brown <tracybrownjr@gmail.com>
2016-08-20 15:01:08 -0400

    Chapter 7
    Diffuse materials

--- main.cpp
+++ main.cpp
@@ -10,23 +10,28 @@
 #include "sphere.h"
 #include "camera.h"
 
+vec3 random_in_unit_sphere() {
+    vec3 p;
+    do {
+        p = 2.f*vec3(drand48(), drand48(), drand48()) - vec3(1.f, 1.f, 1.f);
+    } while(p.squared_length() >= 1.f);
+    return p;
+}
+
 vec3 color(const ray& r, const std::unique_ptr<hitable>& world) {
     hit_record rec;
-    const vec3& center = vec3(0.f, 0.f, -1.f);
-    if (world->hit(r, 0.f, std::numeric_limits<float>::max(), rec)) {
-        return 0.5f*vec3(rec.normal.x+1, rec.normal.y+1, rec.normal.z+1);
+    if (world->hit(r, 0.001f, std::numeric_limits<float>::max(), rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5f*color( ray(rec.p, target - rec.p), world );
     }
     else {
         vec3 unit_direction = unit_vector(r.direction);
         float t = 0.5f*(unit_direction.y + 1.f);
-        return (1.0 - t) * vec3(1.f, 1.f, 1.f) + t*vec3(0.5f, 0.7f, 1.0);
+        return (1.f - t) * vec3(1.f, 1.f, 1.f) + t*vec3(0.5f, 0.7f, 1.f);
     }
 }
 
 int main() {
-    std::random_device random_device;
-    std::mt19937 generator(random_device());
-    std::uniform_real_distribution<float> distribution(0, 1);
 
     int nx(200);
     int ny(100);
@@ -46,13 +51,13 @@ int main() {
         for (int i = 0; i < nx; ++i) {
             vec3 final_color(0.f, 0.f, 0.f);
             for (int s = 0; s < ns; ++s) {
-                float u = (i /*+ distribution(generator)*/) / static_cast<float>(nx);
-                float v = (j /*+ distribution(generator)*/) / static_cast<float>(ny);
+                float u = (i + drand48()) / static_cast<float>(nx);
+                float v = (j + drand48()) / static_cast<float>(ny);
                 ray r = cam.get_ray(u, v);
                 final_color += color(r, world);
             }
             final_color /= static_cast<float>(ns);
-
+            final_color = vec3( sqrt(final_color.x), sqrt(final_color.y), sqrt(final_color.z) );
             int ir = static_cast<int>(255.99 * final_color.r);
             int ig = static_cast<int>(255.99 * final_color.g);
             int ib = static_cast<int>(255.99 * final_color.b);
