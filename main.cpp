23894df26d872bbf8a8f3c6cce755557908d7261
Tracy Brown <tracybrownjr@gmail.com>
2016-09-26 22:41:45 -0400

    First attempt at a rasterizer...still need to work out some kinks

--- main.cpp
+++ main.cpp
@@ -1,60 +1,79 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
-#include <array>
 #include <cstdint>
+#include <chrono>
 
-#include <Eigen/Core>
 #include <Eigen/Geometry>
 
-std::vector<Eigen::Vector4f> vertices = { 
-    {  -2.5703,   0.78053,  -2.4e-05, 1}, { -0.89264,  0.022582,  0.018577, 1}, 
-    {   1.6878, -0.017131,  0.022032, 1}, {   3.4659,  0.025667,  0.018577, 1}, 
-    {  -2.5703,   0.78969, -0.001202, 1}, { -0.89264,   0.25121,   0.93573, 1}, 
-    {   1.6878,   0.25121,    1.1097, 1}, {   3.5031,   0.25293,   0.93573, 1}, 
-    {  -2.5703,    1.0558, -0.001347, 1}, { -0.89264,    1.0558,    1.0487, 1}, 
-    {   1.6878,    1.0558,    1.2437, 1}, {   3.6342,    1.0527,    1.0487, 1}, 
-    {  -2.5703,    1.0558,        0., 1}, { -0.89264,    1.0558,       0., 1}, 
-    {   1.6878,    1.0558,        0., 1}, {   3.6342,    1.0527,       0., 1}, 
-    {  -2.5703,    1.0558,  0.001347, 1}, { -0.89264,    1.0558,   -1.0487, 1}, 
-    {   1.6878,    1.0558,   -1.2437, 1}, {   3.6342,    1.0527,   -1.0487, 1}, 
-    {  -2.5703,   0.78969,  0.001202, 1}, { -0.89264,   0.25121,  -0.93573, 1}, 
-    {   1.6878,   0.25121,   -1.1097, 1}, {   3.5031,   0.25293,  -0.93573, 1}, 
-    {   3.5031,   0.25293,        0., 1}, {  -2.5703,   0.78969,       0., 1}, 
-    {   1.1091,    1.2179,        0., 1}, {    1.145,     6.617,       0., 1}, 
-    {   4.0878,    1.2383,        0., 1}, {  -2.5693,    1.1771, -0.081683, 1}, 
-    {  0.98353,    6.4948, -0.081683, 1}, { -0.72112,    1.1364, -0.081683, 1}, 
-    {   0.9297,     6.454,        0., 1}, {  -0.7929,     1.279,       0., 1}, 
-    {  0.91176,    1.2994,        0., 1} 
-}; 
+#include "cow.h"
 
-const std::vector<uint32_t> triangles = { 
-     4,   0,   5,   0,   1,   5,   1,   2,   5,   5,   2,   6,   3,   7,   2, 
-     2,   7,   6,   5,   9,   4,   4,   9,   8,   5,   6,   9,   9,   6,  10, 
-     7,  11,   6,   6,  11,  10,   9,  13,   8,   8,  13,  12,  10,  14,   9, 
-     9,  14,  13,  10,  11,  14,  14,  11,  15,  17,  16,  13,  12,  13,  16, 
-    13,  14,  17,  17,  14,  18,  15,  19,  14,  14,  19,  18,  16,  17,  20, 
-    20,  17,  21,  18,  22,  17,  17,  22,  21,  18,  19,  22,  22,  19,  23, 
-    20,  21,   0,  21,   1,   0,  22,   2,  21,  21,   2,   1,  22,  23,   2, 
-     2,  23,   3,   3,  23,  24,   3,  24,   7,  24,  23,  15,  15,  23,  19, 
-    24,  15,   7,   7,  15,  11,   0,  25,  20,   0,   4,  25,  20,  25,  16, 
-    16,  25,  12,  25,   4,  12,  12,   4,   8,  26,  27,  28,  29,  30,  31, 
-    32,  34,  33 
-};
+static constexpr auto inchToMillimeters = 25.4f;
+enum class FitResolutionGate { kFill = 0, kOverscan };
 
-bool computePixelCoordinates(
-	const Eigen::Vector4f& worldCoordinate,
-    const Eigen::Matrix4f& worldToCamera,
-    const float& bottom,
-    const float& left,
-    const float& top,
-    const float& right,
-    const float& near,
-    const uint32_t& imageWidth,
-    const uint32_t& imageHeight,
-    Eigen::Vector2i& pixelCoordinate)
+void computeScreenCoordinates(
+    float filmApertureWidth,
+    float filmApertureHeight,
+    uint32_t imageWidth,
+    uint32_t imageHeight,
+    FitResolutionGate fitFilm,
+    float nearClippingPlane,
+    float focalLength,
+    float& top, float& bottom, float& left, float& right
+)
 {
-    auto cameraCoordinate = worldToCamera * worldCoordinate;
+    float filmAspectRatio = filmApertureWidth / filmApertureHeight;
+    float deviceAspectRatio = imageWidth / static_cast<float>(imageHeight);
+
+    top = ((filmApertureHeight * inchToMillimeters / 2 ) / focalLength) * nearClippingPlane;
+    right = ((filmApertureWidth * inchToMillimeters / 2) / focalLength) * nearClippingPlane;
+
+    // field of view (horizontal)
+    auto fov = 2 * 180 / M_PI * atan((filmApertureWidth * inchToMillimeters / 2) / focalLength);
+    std::cerr << "Field of view " << fov << std::endl;
+
+    auto xScale = 1.f;
+    auto yScale = 1.f;
+
+    switch (fitFilm) {
+        default:
+        case FitResolutionGate::kFill:
+            if (filmAspectRatio > deviceAspectRatio) {
+                xScale = deviceAspectRatio / filmAspectRatio;
+            }
+            else {
+                yScale = filmAspectRatio / deviceAspectRatio;
+            }
+            break;
+        case FitResolutionGate::kOverscan:
+            if (filmAspectRatio > deviceAspectRatio) {
+                yScale = filmAspectRatio / deviceAspectRatio;
+            }
+            else {
+                xScale = deviceAspectRatio / filmAspectRatio;
+            }
+            break;
+    }
+
+    right *= xScale;
+    top *= yScale;
+
+    bottom = -top;
+    left = -right;
+}
+
+bool convertToRaster(
+	const Eigen::Vector4f& vertexWorld,
+    const Eigen::Matrix4f& worldToCamera,
+    float bottom,
+    float left,
+    float top,
+    float right,
+    float near,
+    uint32_t imageWidth,
+    uint32_t imageHeight,
+    Eigen::Vector4f& vertexRaster)
+{
+    auto cameraCoordinate = worldToCamera * vertexWorld;
 
     Eigen::Vector2f screenCoordinate{ 
         cameraCoordinate.x() / -cameraCoordinate.z() * near,
@@ -62,92 +81,173 @@ bool computePixelCoordinates(
     };
 
     Eigen::Vector2f ndcCoordinate{
-        (screenCoordinate.x() + right) / (2 * right),
-        (screenCoordinate.y() + top) / (2 * top)
+        2 * screenCoordinate.x() / (right - left) - (right + left) / (right - left),
+        2 * screenCoordinate.y() / (top - bottom) - (top + bottom) / (top - bottom)
     };
 
-    pixelCoordinate.x() = static_cast<int>(ndcCoordinate.x() * imageWidth);
-    pixelCoordinate.y() = static_cast<int>((1 - ndcCoordinate.y()) * imageHeight);
-
-    auto visible = true;
-    if (screenCoordinate.x() < 1 || screenCoordinate.x() > right ||
-        screenCoordinate.y() < bottom || screenCoordinate.y() > top)
-    {
-        visible = false;
-    }
-
-    return visible;
+    vertexRaster.x() = (ndcCoordinate.x() + 1) / 2 * imageWidth;
+    vertexRaster.y() = (1 - ndcCoordinate.y()) / 2 * imageHeight;
 }
 
-auto focalLength = 35.f;
-auto filmApertureWidth = 0.980f;
-auto filmApertureHeight = 0.735f;
-
-static const auto inchToMillimeters = 25.4f;
-
-auto nearClippingPlane = 0.1f;
-auto farClippingPlane = 1000.f;
-
-uint32_t imageWidth = 640;
-uint32_t imageHeight = 480;
-
-enum class FitResolutionGate { kFill = 0, kOverscan };
-FitResolutionGate fitFilm = FitResolutionGate::kOverscan;
-
-float calculateRightCanvasCoord() {
-    return ((filmApertureWidth * inchToMillimeters / 2.f) / focalLength) * nearClippingPlane;
+float min3(float a, float b, float c) { 
+    return std::min(a, std::min(b, c)); 
 }
 
-float calculateTopCanvasCoord() {
-    return ((filmApertureHeight * inchToMillimeters / 2.f) / focalLength) * nearClippingPlane;
+float max3(float a, float b, float c) {
+    return std::max(a, std::max(b, c));
 }
 
-float edgeFunction(const Eigen::Vector2f& a, const Eigen::Vector2f& b, const Eigen::Vector2f& c) {
+float edgeFunction(const Eigen::Vector4f& a, const Eigen::Vector4f& b, const Eigen::Vector4f& c) {
     return (c.x() - a.x()) * (b.y() - a.y()) - (c.y() - a.y()) * (b.x() - a.x());
 }
 
-using Rgb = std::array<char, 3>;
+float edgeFunctionCCW(const Eigen::Vector4f& a, const Eigen::Vector4f& b, const Eigen::Vector4f& c) {
+    return (a.x() - b.x()) * (c.y() - a.y()) - (a.y() - b.y()) * (c.x() - a.x());
+}
+
+constexpr auto imageWidth = 640;
+constexpr auto imageHeight = 480;
+Eigen::Matrix4f worldToCamera;
+
+const auto numberOfTriangles = 3156;
+auto nearClippingPlane = 1.f;
+auto farClippingPlane = 1000.f;
+auto focalLength = 20.f; // in mm
+// 35mm Full Aperture in inches
+auto filmApertureWidth = 0.980f;
+auto filmApertureHeight = 0.735f;
+
+using Rgb = std::array<unsigned char, 3>;
 
 int main() {
-    Eigen::Vector2f v0  = {491.407, 411.407};
-    Eigen::Vector2f v1 = {148.593, 68.5928};
-    Eigen::Vector2f v2 = {148.593, 411.407};
-    Eigen::Vector3f c0 = {1, 0, 0};
-    Eigen::Vector3f c1 = {0, 1, 0};
-    Eigen::Vector3f c2 = {0, 0, 1};
+    // Make the data for this matrix colomn...right now I'm setting the columns
+    // as rows. Should be transposed.
+    worldToCamera << 
+        0.707107, -0.331295, 0.624695, 0, 
+        0, 0.883452, 0.468521, 0, 
+        -0.707107, -0.331295, 0.624695, 0, 
+        -1.63871, -5.74777, -40.400412, 1;
+    worldToCamera.transposeInPlace();
+    
+    Eigen::Matrix4f cameraToWorld = worldToCamera.inverse();
 
-    const uint32_t w = 512;
-    const uint32_t h = 512;
+    float t, b, l, r;
 
-    std::array<Rgb, w * h> framebuffer;
-    framebuffer.fill({0,0,0});
+    computeScreenCoordinates(
+        filmApertureWidth,
+        filmApertureHeight,
+        imageWidth,
+        imageHeight,
+        FitResolutionGate::kOverscan, 
+        nearClippingPlane,
+        focalLength,
+        t, b, l, r);
+    
+    std::array<Rgb, imageWidth * imageHeight> framebuffer;
+    framebuffer.fill({255,255,255});
 
-    auto area = edgeFunction(v0, v1, v2);
+    std::array<float, imageWidth * imageHeight> depthbuffer;
+    depthbuffer.fill(farClippingPlane);
 
-    for (uint32_t j = 0; j < h; ++j) {
-        for (uint32_t i = 0; i < w; ++i) {
-            Eigen::Vector2f p = {i + 0.5f, j + 0.5f};
-            float w0 = edgeFunction(v1, v2, p);
-            float w1 = edgeFunction(v2, v0, p);
-            float w2 = edgeFunction(v0, v1, p);
-            if (w0 >= 0 && w1 >= 0 && w2 >= 0) {
-                w0 /= area;
-                w1 /= area;
-                w2 /= area;
-                float r = w0 * c0.x() + w1 * c1.x() + w2 * c2.x();
-                float g = w0 * c0.y() + w1 * c1.y() + w2 * c2.y();
-                float b = w0 * c0.z() + w1 * c1.z() + w2 * c2.z();
-                framebuffer[j * w + i][0] = static_cast<char>(r * 255);
-                framebuffer[j * w + i][1] = static_cast<char>(g * 255);
-                framebuffer[j * w + i][2] = static_cast<char>(b * 255);
+    auto startTime = std::chrono::high_resolution_clock::now();
+
+    for (auto i = 0; i < numberOfTriangles; ++i) {
+        Eigen::Vector4f v0 = Eigen::Vector4f::UnitW();
+        Eigen::Vector4f v1 = Eigen::Vector4f::UnitW();
+        Eigen::Vector4f v2 = Eigen::Vector4f::UnitW();
+        v0.block<3,1>(0,0) = vertices[nvertices[i * 3]].cast<float>();
+        v1.block<3,1>(0,0) = vertices[nvertices[i * 3 + 1]].cast<float>();
+        v2.block<3,1>(0,0) = vertices[nvertices[i * 3 + 2]].cast<float>();
+
+        Eigen::Vector4f v0Raster, v1Raster, v2Raster;
+
+        convertToRaster(v0, worldToCamera, l, r, t, b, nearClippingPlane, imageWidth, imageHeight, v0Raster);
+        convertToRaster(v1, worldToCamera, l, r, t, b, nearClippingPlane, imageWidth, imageHeight, v1Raster);
+        convertToRaster(v2, worldToCamera, l, r, t, b, nearClippingPlane, imageWidth, imageHeight, v2Raster);
+
+        v0Raster.z() = 1 / v0Raster.z();
+        v1Raster.z() = 1 / v1Raster.z();
+        v2Raster.z() = 1 / v2Raster.z();
+
+        Eigen::Vector2f st0 = st[stindices[i * 3]].cast<float>();
+        Eigen::Vector2f st1 = st[stindices[i * 3 + 1]].cast<float>();
+        Eigen::Vector2f st2 = st[stindices[i * 3 + 2]].cast<float>();
+
+        st0 *= v0Raster.z(), st1 *= v1Raster.z(), st2 *= v2Raster.z();
+
+        auto xmin = min3(v0Raster.x(), v1Raster.x(), v2Raster.x());
+        auto ymin = min3(v0Raster.y(), v1Raster.y(), v2Raster.y());
+        auto xmax = max3(v0Raster.x(), v1Raster.x(), v2Raster.x());
+        auto ymax = max3(v0Raster.y(), v1Raster.y(), v2Raster.y());
+
+        if (xmin > imageWidth - 1 || xmax < 0 || ymin > imageHeight - 1 || ymax < 0)
+            continue;
+
+        auto x0 = std::max(0, static_cast<int32_t>(std::floor(xmin)));
+        auto x1 = std::min(imageWidth - 1, static_cast<int32_t>(std::floor(xmax)));
+        auto y0 = std::max(0, static_cast<int32_t>(std::floor(ymin)));
+        auto y1 = std::min(imageHeight - 1, static_cast<int32_t>(std::floor(ymax)));
+
+        auto area = edgeFunction(v0, v1, v2);
+
+        for (uint32_t y = y0; y <= y1; ++y) {
+            for (uint32_t x = x0; x <= x1; ++x) {
+                Eigen::Vector4f pixelSample = {x + 0.5f, y + 0.5f, 0.f, 1.f};
+                auto w0 = edgeFunction(v1Raster, v2Raster, pixelSample);
+                auto w1 = edgeFunction(v2Raster, v0Raster, pixelSample);
+                auto w2 = edgeFunction(v0Raster, v1Raster, pixelSample);
+                if (w0 >= 0 && w1 >= 0 && w2 >= 0) {
+                    w0 /= area;
+                    w1 /= area;
+                    w2 /= area;
+                    auto oneOverZ = v0Raster.z() * w0 + v1Raster.z() * w1 + v2Raster.z() * w2;
+                    auto z = 1 / oneOverZ;
+
+                    if (z < depthbuffer[y * imageWidth + x]) {
+                        depthbuffer[y * imageWidth + x] = z;
+
+                        Eigen::Vector2f st = st0 * w0 + st1 * w1 + st2 * w2;
+
+                        st *= z;
+
+                        Eigen::Vector4f v0Cam = worldToCamera * v0;
+                        Eigen::Vector4f v1Cam = worldToCamera * v1;
+                        Eigen::Vector4f v2Cam = worldToCamera * v2;
+
+                        auto px = (v0Cam.x() / -v0Cam.z()) * w0 + (v1Cam.x() / -v1Cam.z()) * w1 + (v2Cam.x() / -v2Cam.z()) * w2;
+                        auto py = (v0Cam.y() / -v0Cam.z()) * w0 + (v1Cam.y() / -v1Cam.z()) * w1 + (v2Cam.y() / -v2Cam.z()) * w2;
+                        Eigen::Vector4f pt{px * z, py * z, -z, 0.f};
+
+                        Eigen::Vector4f a = (v1Cam - v0Cam);
+                        Eigen::Vector4f b = (v2Cam - v0Cam);
+                        Eigen::Vector4f n = a.cross3(b);
+                        n.normalize();
+                        Eigen::Vector4f viewDirection = -pt;
+                        viewDirection.normalize();
+
+                        auto nDotView = std::max(0.f, n.dot(viewDirection));
+
+                        const auto M = 10;
+                        auto checker = (fmod(st.x() * M, 1.f) > 0.5f) ^ (fmod(st.y() + M, 1.f) < 0.5f);
+                        auto c = 0.3f * (1.f - checker) + 0.7f * checker;
+                        nDotView *= c;
+                        framebuffer[y * imageWidth + x][0] = nDotView * 255;
+                        framebuffer[y * imageWidth + x][1] = nDotView * 255;
+                        framebuffer[y * imageWidth + x][2] = nDotView * 255;
+                    }
+                }
             }
         }
     }
 
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto passedTime = std::chrono::duration<double, std::milli>(endTime - startTime).count();
+    std::cerr << "Wall passed time: " << passedTime << " ms" << std::endl;
+
     std::ofstream imageFile;
-    imageFile.open("./raster2d.ppm");
-    imageFile << "P6\n" << w << " " << h << "\n255\n";
-    imageFile.write(framebuffer.front().data(), w * h * 3);
+    imageFile.open("./output.ppm");
+    imageFile << "P6\n" << imageWidth << " " << imageHeight << "\n255\n";
+    imageFile.write(reinterpret_cast<char *>(framebuffer.front().data()), imageWidth * imageHeight * 3);
     imageFile.close();
 
     return 0;
